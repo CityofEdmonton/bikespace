d21afe3142986a340722d51c702c03e8ab1753b3
Jared Rewerts <jaredrewerts@gmail.com>
2019-02-22 13:40:31 -0700

    Removed generated template files.

--- bicycleparking/copy.sh
+++ bicycleparking/copy.sh
@@ -2,4 +2,5 @@
 mkdir -p ./templates/bicycleparking
 cp -f ./static/dist/index.html ./templates/bicycleparking/
 cp -f ./static/dist/sw.js ./templates/bicycleparking/
+cp -f ./static/dist/manifest.json ./templates/bicycleparking/
 cp -f ./static/dist/bundle.js ./static/
