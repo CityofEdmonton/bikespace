aa9f7613dab78ab76d160f19427ecf1ba821f978
Jared Rewerts <jaredrewerts@gmail.com>
2019-02-23 12:05:29 -0700

    Clean up old comments and prints.

--- bicycleparking/photos/s3uploader.py
+++ bicycleparking/photos/s3uploader.py
@@ -9,22 +9,17 @@ class S3Uploader(PhotoUploader):
     """This class is used for upload picture to S3"""
 
     def __init__(self):
-        print(settings.AWS_ACCESS_KEY_ID)
         self.client = boto3.client(
             's3',
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
     def write(self, filename, file):
-        # stat = os.stat (filename)
-        # if stat.st_size < 2097152 :
         key = str(uuid.uuid1()) + os.path.splitext(filename)[1]
         self.client.put_object(Bucket=settings.S3_BUCKET,
                                Key=key,
                                Body=file.read())
         return key
-        # else :
-        #   return ""
 
     def read(self, key):
         return self.client.get_object(Bucket=settings.S3_BUCKET, Key=key)['Body'].read()
